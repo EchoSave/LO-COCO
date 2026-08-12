6b69cd5de7b967ece143b2f95c4629425d6200ca
EchoSave <echo.salonga@edu.sait.ca>
2026-08-12 14:59:41 -0600

    fixing error on _layout

---
+++ lo-coco/global.d.ts
@@ -0,0 +1,9 @@
+// Type declarations for importing CSS and stylesheet files in TypeScript
+// Allows side-effect imports like `import "./globals.css"` in Next.js
+
+declare module '*.css';
+declare module '*.scss';
+declare module '*.sass';
+declare module '*.module.css';
+declare module '*.module.scss';
+declare module '*.module.sass';
